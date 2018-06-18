52d8635042faebcd8bc067858c09032372dff9a5
Nelly Delessy <ndelessy@mdc.edu>
2018-06-17 20:45:10 -0400

    Room database

--- app/src/main/java/edu/mdc/north/cop4656/cop4656labs/MainActivity.java
+++ app/src/main/java/edu/mdc/north/cop4656/cop4656labs/MainActivity.java
@@ -1,11 +1,11 @@
 package edu.mdc.north.cop4656.cop4656labs;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -250,9 +250,30 @@ public class MainActivity extends AppCompatActivity
             outputStream.write(reminderText.getText().toString().concat("\n").getBytes());
             outputStream.close();
             Toast.makeText(this, "Reminder added!", Toast.LENGTH_LONG).show();
-            reminderText.setText("");
+            reminderText.getText().clear();
         } catch (Exception e) {
             Log.w("WARNING", "Cannot save reminder..");
         }
+    }
+
+    public void addThing(View view) {
+        final AppDatabase mDb = AppDatabase.getInstance(MainActivity.this);
+        final EditText nameText = (EditText) findViewById(R.id.nameText);
+        final EditText attributeText = (EditText) findViewById(R.id.attributeText);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(final Void ... params ) {
+                mDb.thingModel().insert(new Thing(nameText.getText().toString(), attributeText.getText().toString()));
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute( final Void result ) {
+                nameText.getText().clear();
+                attributeText.getText().clear();
+            }
+        }.execute();
+
     }
 }
